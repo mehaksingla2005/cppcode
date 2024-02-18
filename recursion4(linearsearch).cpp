97b1678662bf626fda1302a9701c467d8eece5e5
mehaksingla2005 <singlamehak2005@gmail.com>
2024-02-18 11:24:24 +0530

    Update recursion4(linearsearch).cpp

--- recursion4(linearsearch).cpp
+++ recursion4(linearsearch).cpp
@@ -1,6 +1,13 @@
 #include<iostream>
 using namespace std;
+void print(int arr[],int n){
+    cout<<"size of array is "<<n<<endl;
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }cout<<endl;
+}
 bool checkFound(int arr[0],int key,int size){
+    print(arr,size);
     if(size==0){
         return false;
     }
@@ -13,7 +20,7 @@ bool remainingPart=checkFound(arr+1,key,size-1);
 }
 int main(){
     int arr[5]={3,2,1,6,5};
-    bool ans=checkFound(arr,1,5);
+    bool ans=checkFound(arr,6,5);
     if(ans){
         cout<<"found";
     }else{
