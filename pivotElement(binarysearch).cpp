a7fa2bf5e4345f3338391c42fd58acdfb8f34b0e
mehaksingla2005 <singlamehak2005@gmail.com>
2024-02-19 11:51:16 +0530

    Update pivotElement(binarysearch).cpp

--- pivotElement(binarysearch).cpp
+++ pivotElement(binarysearch).cpp
@@ -1,20 +1,21 @@
-// Online C++ compiler to run C++ program online
+
 #include <iostream>
 using namespace std;
 int pivot(int *arr,int n){
     int s=0;
     int e=n-1;
-    int mid=s+(e-s)/2;
+    
     while(s<e){
+        int mid=s+(e-s)/2;
         if(arr[mid]>=arr[0]){
             s=mid+1;
         }else e=mid;
-        return mid;
-    }
+ 
+    } return s;//return e
 }
 
 int main() {
-    int arr[5]={7,9,1,2,3};
+    int arr[5]={3,8,10,17,1};
     cout<<"pivot element is at index: "<<pivot(arr,5);
 
     return 0;
