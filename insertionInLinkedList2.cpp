bbf2a4504f26c2fb93313921defebc54637c9e2f
mehaksingla2005 <singlamehak2005@gmail.com>
2024-04-12 17:34:42 +0530

    insertion at end and at position

---
+++ insertionInLinkedList2.cpp
@@ -0,0 +1,69 @@
+// insertion at the end and insertion at position
+#include <iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node(int data){
+        this->data=data;
+        this->next=NULL;
+}};
+void insertAtHead(Node* &head,int d){
+    //new node create
+    Node* temp = new Node(d);
+    temp->next=head;
+    head=temp;
+}
+void insertAtTail(Node* &tail,int d){
+    Node* temp = new Node(d);
+    tail->next=temp;
+    tail=temp;
+}
+void print(Node* &head){
+    Node* temp=head;
+    while(temp!=NULL){
+     cout<<temp->data<<" ";
+     temp=temp->next  ; 
+    }
+    cout<<endl;
+    
+}
+
+void insertAtPosition(Node* &head,int position,int d){
+    if(position==1){
+        insertAtHead(head,d);
+    }else{
+    Node* temp=head;
+    int cnt=1;
+    while(cnt<position-1){
+        temp=temp->next;
+        cnt++;
+    }
+    Node* nodeToInsert=new Node(d);
+    nodeToInsert->next=temp->next;
+    temp->next=nodeToInsert;
+}}
+
+int main() {
+    //create a new node
+    Node* node1= new Node(10);
+    cout<<node1->data<<endl;
+    cout<<node1->next<<endl;
+    
+    //head pointed to node1
+    Node* head =node1;
+    Node* tail=node1;
+    print(head);
+    insertAtHead(head,15);
+        print(head);
+        insertAtTail(tail,12);
+        print(head);
+        insertAtPosition(head,3,8);
+        print(head);
+        insertAtPosition(head,1,8);
+        print(head);
+        
+        
+    return 0;
+}
