a94baff7b4c5cfde1c0b9a5069be46b2bce21ca3
mehaksingla2005 <singlamehak2005@gmail.com>
2024-06-07 10:57:03 +0530

    made changes using map

--- longestsubstring.cpp
+++ longestsubstring.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int maxlen=0;
-        int n=s.size();
-        map<int,int> mpp;
-        int left=0;
-        int right=0;
-        while(left<=right&&right<n){
-           
-            if(mpp.find(s[right])==mpp.end()){
-                 int len=right-left+1;
-             maxlen=max(len,maxlen);
-                 mpp.insert({s[right],right});
-                 right++;
-                
-            }else{
-                left=mpp[s[right]]+1;
-                mpp[s[right]]=right;
-                  int len=right-left+1;
-             maxlen=max(len,maxlen);
-             right++;   
-            }
-           
+        int maxlen = 0;
+        int n = s.size();
+        map<char, int> mpp;  // Use char as the key type
+        int left = 0;
+        int right = 0;
 
+        while (right < n) {
+            if (mpp.find(s[right]) == mpp.end() || mpp[s[right]] < left) {
+                // Either the character is not in the map or it's not in the current window
+                mpp[s[right]] = right;  // Update the character's position
+                int len = right - left + 1;
+                maxlen = max(len, maxlen);
+                right++;
+            } else {
+                // Character is in the current window, move the left pointer
+                left = mpp[s[right]] + 1;  // Move left pointer to the right of the duplicate character
+                mpp[s[right]] = right;  // Update the character's position
+                right++;
+            }
         }
+
         return maxlen;
-        
     }
 };
