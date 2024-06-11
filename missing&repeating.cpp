5e3eaa1812d506448a1711cd00a8fd415654c50c
mehaksingla2005 <singlamehak2005@gmail.com>
2024-06-12 01:40:25 +0530

    Create missing&repeating.cpp

---
+++ missing&repeating.cpp
@@ -0,0 +1,17 @@
+vector<int> findTwoElement(vector<int> arr, int n) {
+        // code here
+        int hash[n+1]={0};
+        for(int i=0;i<n;i++){
+            hash[arr[i]]++;
+        }
+        int repeating=-1;
+        int missing=-1;
+        for(int i=1;i<=n;i++){
+            if(hash[i]==2)repeating=i;
+            else if(hash[i]==0)missing=i;
+            if(repeating!=-1 && missing !=-1){
+                break;
+            }
+            
+        }return {repeating,missing};
+    }
