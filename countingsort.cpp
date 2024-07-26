45df6a991b4fffcc54bcb493390c8f110cc861ba
Mehak Singla <singlamehak2005@gmail.com>
2024-07-25 23:31:08 -0700

    O(n+k)

---
+++ countingsort.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        for(int& num:nums){
+            mp[num]++;
+        }
+        int mini=*min_element(nums.begin(),nums.end());
+        int maxi=*max_element(nums.begin(),nums.end());
+        int i=0;
+        for(int num=mini;num<=maxi;num++){
+            while(mp[num]>0){
+                nums[i]=num;
+                i++;
+                mp[num]--;
+            }
+        }
+        return nums;
+    }
+};
