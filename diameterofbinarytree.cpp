af0aedf6e63e20f0ac391ce5f5d5585352689acf
Mehak Singla <singlamehak2005@gmail.com>
2024-07-29 07:25:29 -0700

    O(n)

---
+++ diameterofbinarytree.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    int height(TreeNode* root,int &maxi){
+       if(root==NULL)return 0;
+        
+        int lh=height(root->left,maxi);
+        int rh=height(root->right,maxi);
+        maxi=max(maxi,lh+rh);
+        return 1+max(lh,rh);
+    }
+ 
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        int maxi=0;
+        height(root,maxi);
+        return maxi;
+    }
+};
