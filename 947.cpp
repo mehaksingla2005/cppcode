797938006befe8f58708dc8e70d8a53ebee4d90c
Mehak Singla <singlamehak2005@gmail.com>
2024-08-30 19:21:27 +0530

    O(n^2)

--- 947.cpp
+++ 947.cpp
@@ -31,3 +31,36 @@ public:
         return n-count;
     }
 };
+
+
+
+//same approach but diff code
+class Solution {
+private:
+    void dfs(vector<vector<int>>& stones,int index,vector<bool> &vis){
+        vis[index]=true;
+        int n=stones.size();
+        for(int i=0;i<n;i++){
+       
+                int r=stones[index][0];
+                int c=stones[index][1];
+                if(!vis[i] && (stones[i][0]==r || stones[i][1]==c)){
+                    dfs(stones,i,vis);
+                }
+            
+        }
+    }
+public:
+    int removeStones(vector<vector<int>>& stones) {
+        int n=stones.size();
+        int cnt=0;
+        vector<bool> vis(n,false);
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                dfs(stones,i,vis);
+                cnt++;
+            }
+        }
+        return n-cnt;
+    }
+};
