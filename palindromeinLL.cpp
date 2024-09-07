3f7d4efebe298935f6eec2a8471f434f31d74fee
Mehak Singla <singlamehak2005@gmail.com>
2024-09-07 12:33:25 +0530

    Create palindromeinLL.cpp

---
+++ palindromeinLL.cpp
@@ -0,0 +1,69 @@
+
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        stack<int> st;
+        ListNode* temp=head;
+        while(temp!=NULL){
+            st.push(temp->val);
+            temp=temp->next;
+        }
+        temp=head;
+        while(temp!=NULL){
+            if(temp->val!=st.top()){
+                return false;
+            }
+            st.pop();
+            temp=temp->next;
+        }
+        return true;
+    }
+};
+
+
+//2nd approach
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+private:
+    ListNode* reverseLL(ListNode* head){
+        if(head==NULL|| head->next==NULL)return head;
+        ListNode* newHead=reverseLL(head->next);
+        ListNode* front=head->next;
+        front->next=head;
+        head->next=NULL;
+        return newHead;
+    }
+public:
+    bool isPalindrome(ListNode* head) {
+        if(head==NULL|| head->next==NULL)return head;
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast->next!=NULL && fast->next->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        ListNode* newHead=reverseLL(slow->next);
+        ListNode* first=head;
+        ListNode* second=newHead;
+        while(second!=NULL){
+            if(first->val!=second->val){
+                reverseLL(newHead);
+                return false;
+            }
+            first=first->next;
+            second=second->next;
+        }
+        reverseLL(newHead);
+        return true;
+    }
+};
