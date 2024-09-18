f133d0ae83967f1052c78f8ffb5931a50c397947
Mehak Singla <singlamehak2005@gmail.com>
2024-09-18 17:35:13 +0530

    Create lengthOfLastWord.cpp

---
+++ lengthOfLastWord.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int n=s.length();
+        int i=n-1;
+        while(i>=0 && s[i]==' ')i--;
+        int length=0;
+        while(i>=0 && s[i]!=' '){
+            i--;
+            length++;
+        }
+        return length;
+    }
+};
+
+//Using c++ stl
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int idx=s.find_last_not_of(' ')+1;
+        s.erase(idx);
+        int last_space_idx=s.find_last_of(' ');
+        return last_space_idx==string::npos?s.length():s.length()-last_space_idx-1;
+    }
+};
