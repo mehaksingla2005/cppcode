89779416a64bec5652a39dc1bdc63ff81a9da220
Mehak Singla <singlamehak2005@gmail.com>
2024-09-22 22:06:45 +0530

    Update countSubstringsThatCanBerearrangedtoformstring.cpp

--- countSubstringsThatCanBerearrangedtoformstring.cpp
+++ countSubstringsThatCanBerearrangedtoformstring.cpp
@@ -1,35 +1,31 @@
 class Solution {
 public:
-    bool isValid(unordered_map<char,int> word2freq,unordered_map<char,int> windowFreq){
-        for(auto it:word2freq){
-            if(windowFreq[it.first]<it.second)return false;
+    bool isValid(vector<int>& arr2,vector<int>& arr1){
+        for(int i=0;i<26;i++){
+            if(arr2[i]<arr1[i])return false;
         }
         return true;
     }
     long long validSubstringCount(string word1, string word2) {
         int n=word1.length();
         int m=word2.length();
-        if(m>n)return 0;
-        unordered_map<char,int> word2freq;
-        for(auto it:word2){
-            word2freq[it]++;
+        if(n<m)return 0;
+        long long result=0;
+        vector<int> arr1(26,0);
+        for(int i=0;i<m;i++){
+            arr1[word2[i]-'a']++;
         }
-        
-        unordered_map<char,int> windowFreq;
-        int cnt=0;
         int left=0;
-        for(int i=0;i<n;i++){
-            windowFreq[word1[i]]++;
-            while(isValid(word2freq,windowFreq)){
-                cnt+=n-i;
-                windowFreq[word1[left]]--;
+        vector<int> arr2(26,0);
+        for(int right=0;right<n;right++){
+            arr2[word1[right]-'a']++;
+            while(isValid(arr2,arr1)){
+                result+=n-right;
+                arr2[word1[left]-'a']--;
                 left++;
             }
+
         }
-      
-        
-        return cnt;
-        
-        
+        return result;
     }
 };
